3c14f1c6af0a96c9e53aa67efbdeb5b56769ad0b
U-MAIN\e080099 <e080099@MJ15XNP.MAIN.AD.TMH.ORG>
2018-06-11 10:31:30 -0400

    foobar

--- foo.py
+++ foo.py
@@ -1,37 +1,12 @@
-#!/usr/bin/python
+import subprocess as proc
+def runcmd(c):
+    output = proc.getoutput(c)
+    print(output)
 
-def vinsert(s,fword,iword):
-    i = s.find(fword)
-    return (s[:i] + iword + ' ' + s[i:])
+runcmd('ls')
 
-mystr = 'one two three'
-mystr = vinsert(mystr,'two','seven')
-print(mystr)
+runcmd('ls')
 
-def greet(*names):
-    for name in names:
-        print(name)
+output = proc.getoutput(c)
 
-greet('peter','paul')
-
-def fact(i):
-    if i == 0:
-        return 1
-    return i * fact(i - 1)
-
-print(fact(4))
-
-m = [[0 for i in range(3)] for j in range(3)]
-for i in range(3):
-    for j in range(3):
-        m[i][j] = 7
-
-l = [' foo ','bar   ', '  baz ']
-l = [s.strip() for s in l]
-print(l)
-
-l = []
-f = open('myfile','rU')
-for line in f:
-    l.append(line.strip().split())
 
