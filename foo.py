e9784915906d5f959deab770faae830d72db5e3c
U-MAIN\E080099 <E080099@MJ15XNP.MAIN.AD.TMH.ORG>
2018-07-02 10:53:43 -0400

    foobar

--- foo.py
+++ foo.py
@@ -1,12 +1,38 @@
-import subprocess as proc
-def runcmd(c):
-    output = proc.getoutput(c)
-    print(output)
 
-runcmd('ls')
+l = [True,False]
+print(any(l))
+print(all(l))
 
-runcmd('ls')
+def powtwo(x):
+    i = 0
+    while True:
+        yield 2**i
+        i += 1
 
-output = proc.getoutput(c)
+f = powtwo(10)
+for i in range(10):
+    print(next(f))
+
+try:
+    f = open('foofile.txt','rU')
+except:
+    print('No file found.')
+
+a = [1,2,3]
+b = [7,8,9]
+print(list(zip(a,b)))
+z = zip(a,b)
+print(list(zip(*z)))
+
+m = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+z = list(zip(m.values(),m.keys()))
+m.clear()
+for x in z:
+    m[x[0]] = x[1]
+
+print(m)
+
+d = {x: 2**x for x in range(5)}
+print(d)
 
 
